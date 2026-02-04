d86cec2d169b5eb8f3b9eed2a528a906d9b89b8e
Breyner Guarin <breynerguarin10@gmail.com>
2026-02-04 09:11:20 -0500

    Add files via upload

---
+++ zona_de_codgio.py
@@ -0,0 +1,7 @@
+#zona de codigo principal 
+
+from modelo_empleado import Empleado_modelo
+objeto_empleado = Empleado_modelo ()
+dato_producto=objeto_empleado.visualizar_producto()
+print(dato_producto)
+
