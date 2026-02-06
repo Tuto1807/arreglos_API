4afcab2bb2dd797b0d35b93d8e2c29ebfecd7d19
Breyner Guarin <breynerguarin10@gmail.com>
2026-02-06 09:07:51 -0500

    Delete zona_de_codgio.py

--- zona_de_codgio.py
+++
@@ -1,9 +0,0 @@
-#zona de codigo principal 
-
-from modelo_empleado import Empleado_modelo
-objeto_empleado = Empleado_modelo ()
-dato_producto=objeto_empleado.visualizar_producto()
-print(dato_producto)
-
-
-  
